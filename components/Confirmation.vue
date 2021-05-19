c1c877faa44a683e267f3ac950a1cb82f607b493
nari1985 <outsider07.k@gmail.com>
2021-05-20 07:10:59 +0900

    css修正

--- components/Confirmation.vue
+++ components/Confirmation.vue
@@ -16,16 +16,3 @@
       }
   }
 </script>
-
-<style>
-  .component-wrap {
-    width: 90%;
-    max-width: 800px;
-    margin: 15px auto;
-    padding: 20px;
-    background-color: #FFF;
-    border: solid 1px #F1F1F1;
-    border-radius: 5px;
-    box-shadow: 3px 3px 10px rgba(0,0,0,0.2);
-  }
-</style>
--- pages/confirm.vue
+++ pages/confirm.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
-    <Header />
+    <Header/>
     <h1>ご注文内容の確認</h1>
     <div class="component-wrap">
       <p v-for="confirm in confirms">
-        <Confirmation :name="confirm.name" :itemNumber="confirm.itemNumber" :price="confirm.price" />
+        <Confirmation :name="confirm.name" :itemNumber="confirm.itemNumber" :price="confirm.price"/>
       </p>
     </div>
     <p class="total">
@@ -12,37 +12,47 @@
     </p>
   </div>
 </template>
-
 <script>
-export default {
-    data() {
-        return {
-            confirms:[
-                {
-                    name: 'くつした',
-                    itemNumber: 3,
-                    price: 290,
-                },
-                {
-                    name: 'Tシャツ',
-                    itemNumber: 2,
-                    price: 1490,
-                },
-                {
-                    name: 'スカート',
-                    itemNumber: 1,
-                    price: 1990,
-                }
-            ]
+    export default {
+        data() {
+            return {
+                confirms: [
+                    {
+                        name: 'くつした',
+                        itemNumber: 3,
+                        price: 290,
+                    },
+                    {
+                        name: 'Tシャツ',
+                        itemNumber: 2,
+                        price: 1490,
+                    },
+                    {
+                        name: 'スカート',
+                        itemNumber: 1,
+                        price: 1990,
+                    }
+                ]
+            }
         }
     }
-}
 </script>
 <style scoped>
   .container {
     text-align: center;
   }
-
+  
+  .component-wrap {
+    width: 90%;
+    max-width: 800px;
+    margin: 15px auto;
+    padding: 20px;
+    background-color: #FFF;
+    border: solid 1px #F1F1F1;
+    border-radius: 5px;
+    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+  }
+  
   .container .component-wrap:last-of-type {
     margin-bottom: 30px;
   }
@@ -56,5 +66,4 @@ export default {
     font-size: 1.2rem;
     margin-top: 20px;
   }
-  
 </style>
